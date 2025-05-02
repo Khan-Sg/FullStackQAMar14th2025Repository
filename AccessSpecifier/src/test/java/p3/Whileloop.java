7b1ee6c1eb6a3d91ed8d05c634c7d7d7976b2b06
Khan-Sg <abuzar.ahamed.khan@gmail.com>
2025-05-02 13:17:39 +0530

    program

---
+++ AccessSpecifier/src/test/java/p3/Whileloop.java
@@ -0,0 +1,13 @@
+package p3;
+
+public class Whileloop {
+    public static void main(String[] args) {
+        int i=1,num=5;
+
+            while (i <= 10){
+                System.out.println(num + "*" + i + "=" + (num* i));
+                i++;
+            }
+        }
+
+}
